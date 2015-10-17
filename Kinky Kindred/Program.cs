9845c88ebddf8f4c30770bbb880765cf3ac0fe78
ststatus <status>
2015-10-17 14:23:59 -0400

    xcxooxl crit ult

--- Kinky Kindred/Program.cs
+++ Kinky Kindred/Program.cs
@@ -67,7 +67,8 @@ namespace Kinky_Kindred {
             SmiteM.AddItem(new MenuItem("smite_r_if__dying", "Always Smite Red if health < %", true).SetValue(new Slider(15, 1, 100)));
             SmiteM.AddItem(new MenuItem("always_smite_blue", "Always Smite Blue", true).SetValue(false));
             SmiteM.AddItem(new MenuItem("always_smite_frog", "Always Smite Frog", true).SetValue(false));
-            SmiteM.AddItem(new MenuItem("smite_brf_til_lvl", "Only Smite Red/Blue/Frog til level:", true).SetValue(new Slider(10, 1, 18)));
+            SmiteM.AddItem(new MenuItem("smite_brf_til_lvlonoff", "Always Smite Red/Blue/Frog til level:", true).SetValue(true));
+            SmiteM.AddItem(new MenuItem("smite_brf_til_lvl", "", true).SetValue(new Slider(10, 1, 18)));
             SmiteM.AddItem(new MenuItem("always_smite_wolf", "Always Smite Wolves", true).SetValue(false));
             SmiteM.AddItem(new MenuItem("always_smite_golems", "Always Smite Golems", true).SetValue(true));
             SmiteM.AddItem(new MenuItem("always_smite_wraiths", "Always Smite Wraiths", true).SetValue(true));
@@ -76,11 +77,11 @@ namespace Kinky_Kindred {
             SmiteM.AddItem(new MenuItem("smite_ks", "Kill Steal Smite", true).SetValue(true));
 
             JungM.AddItem(new MenuItem("jungleclearQ", "Use Q", true).SetValue(true));
-            JungM.AddItem(new MenuItem("jungleclearmanaminQ", "Q requires % mana", true).SetValue(new Slider(25, 0, 100)));
+            JungM.AddItem(new MenuItem("jungleclearmanaminQ", "Q requires % mana", true).SetValue(new Slider(15, 0, 100)));
             JungM.AddItem(new MenuItem("jungleclearE", "Use E", true).SetValue(true));
             JungM.AddItem(new MenuItem("jungleclearmanaminE", "E requires % mana", true).SetValue(new Slider(35, 0, 100)));
             JungM.AddItem(new MenuItem("jungleclearW", "Use W", true).SetValue(true));
-            JungM.AddItem(new MenuItem("jungleclearmanaminW", "W requires % mana", true).SetValue(new Slider(40, 0, 100)));
+            JungM.AddItem(new MenuItem("jungleclearmanaminW", "W requires % mana", true).SetValue(new Slider(15, 0, 100)));
             JungM.AddItem(new MenuItem("jungleActive", "Active", true).SetValue(true));
 
             LaneM.AddItem(new MenuItem("laneclearQ", "Use Q", true).SetValue(true));
@@ -92,14 +93,21 @@ namespace Kinky_Kindred {
             LaneM.AddItem(new MenuItem("laneclearEsuper", "Use E to kill siege/super if Q isnt avail", true).SetValue(true));
             LaneM.AddItem(new MenuItem("laneclearmanaminE", "E requires % mana", true).SetValue(new Slider(45, 0, 100)));
             LaneM.AddItem(new MenuItem("laneclearW", "Use W", true).SetValue(true));
-            LaneM.AddItem(new MenuItem("laneclearWminminions", "W requires X minions", true).SetValue(new Slider(5, 3, 10)));
+            LaneM.AddItem(new MenuItem("laneclearWminminions", "W requires X minions", true).SetValue(new Slider(6, 3, 10)));
             LaneM.AddItem(new MenuItem("laneclearmanaminW", "W requires % mana", true).SetValue(new Slider(65, 0, 100)));
+            LaneM.AddItem(new MenuItem("laneclearFAST", "Clear using q/w if lots of minions", true).SetValue(true));
+            LaneM.AddItem(new MenuItem("laneclearFASTminions", "Number of minions in range to activate ClearFast", true).SetValue(new Slider(15, 10, 100)));
+            LaneM.AddItem(new MenuItem("laneclearFASTmana", "ClearFast requires % mana", true).SetValue(new Slider(15, 0, 100)));
 
 
             MiscM.AddItem(new MenuItem("killsteal", "Kill Steal", true).SetValue(true));
             MiscM.AddItem(new MenuItem("fleeKey", "Flee Toggle").SetValue(new KeyBind("T".ToCharArray()[0], KeyBindType.Press)));
             MiscM.AddItem(new MenuItem("saveallies", "Save Allies (With R)", true).SetValue(true));
             MiscM.AddItem(new MenuItem("saveallieswhen", "Save when health < %", true).SetValue(new Slider(25, 0, 100)));
+            Menu DontsM = MiscM.AddSubMenu(new Menu("Dont waste ult on:", "Dont waste ult on:"));
+            foreach (var ally in HeroManager.Allies.FindAll(x => x.ChampionName != Player.ChampionName)) {
+                DontsM.AddItem(new MenuItem("target" + ally.ChampionName, ally.ChampionName).SetValue(false));
+            }
 
             kinkykmenu.AddToMainMenu();
         }
@@ -160,8 +168,8 @@ namespace Kinky_Kindred {
             var Qmana = kinm.Item("jungleclearmanaminQ", true).GetValue<Slider>().Value;
             var Wmana = kinm.Item("jungleclearmanaminW", true).GetValue<Slider>().Value;
             var Emana = kinm.Item("jungleclearmanaminE", true).GetValue<Slider>().Value;
-            var MINIONS = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.All, MinionOrderTypes.MaxHealth);
-            var jungleinside = MINIONS.Find(X => X.Team == GameObjectTeam.Neutral && !X.CharData.BaseSkinName.ToLower().Contains("dragon") && !X.CharData.BaseSkinName.ToLower().Contains("baron"));
+            var MINIONS = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
+            var jungleinside = MINIONS.Find(x => x.CharData.BaseSkinName.ToLower().Contains("sru"));
             if (jungleinside != null) {
                 if (kinm.Item("jungleclearQ", true).GetValue<Boolean>() && Manapercent > Qmana) {
                     if (Q.IsReady()) { Q.Cast(Game.CursorPos); }
@@ -180,12 +188,13 @@ namespace Kinky_Kindred {
             var Qmana = kinm.Item("laneclearmanaminQ", true).GetValue<Slider>().Value;
             var Wmana = kinm.Item("laneclearmanaminW", true).GetValue<Slider>().Value;
             var Emana = kinm.Item("laneclearmanaminE", true).GetValue<Slider>().Value;
-            var Minions = MinionManager.GetMinions(Player.ServerPosition, W.Range, MinionTypes.All, MinionTeam.Enemy);
-            if (Minions.Count <= 0) { return; }
+            var clearFAST = kinm.Item("laneclearFASTmana", true).GetValue<Slider>().Value;
+            var MINIONS = MinionManager.GetMinions(W.Range,MinionTypes.All,MinionTeam.Enemy);
+            if (MINIONS.Count <= 0) { return; }
 
             if (kinm.Item("laneclearQ", true).GetValue<Boolean>() && Q.IsReady() && Manapercent >= Qmana) {
                 //try to get as many minions as possible
-                var qminions = Minions.FindAll(x => x.ServerPosition.Distance(Game.CursorPos) < 400f && x.Health < Q.GetDamage(x));
+                var qminions = MINIONS.FindAll(x => x.Position.Distance(Game.CursorPos) < 400f && x.Health < Q.GetDamage(x) && x.ServerPosition.Distance(Player.ServerPosition) < 340f);
                 var siege = qminions.Find(x => x.CharData.BaseSkinName.ToLower().Contains("siege") || x.CharData.BaseSkinName.ToLower().Contains("super"));
                 if (qminions.Count >= kinm.Item("laneclearQcast", true).GetValue<Slider>().Value) { Q.Cast(qminions.FirstOrDefault().ServerPosition); }
                 //try to get sieges if Q is still available
@@ -194,14 +203,18 @@ namespace Kinky_Kindred {
                 }
             }
             if (kinm.Item("laneclearEsuper", true).GetValue<Boolean>() && E.IsReady() && Manapercent >= Emana && !Q.IsReady()) {
-                var siege = Minions.Find(x => x.CharData.BaseSkinName.ToLower().Contains("siege") || x.CharData.BaseSkinName.ToLower().Contains("super"));
+                var siege = MINIONS.Find(x => x.CharData.BaseSkinName.ToLower().Contains("siege") || x.CharData.BaseSkinName.ToLower().Contains("super"));
                 if (siege != null && E.CanCast(siege)) {
                     E.Cast(siege);
                 }
             }
             if (kinm.Item("laneclearW", true).GetValue<Boolean>() && W.IsReady() && Manapercent >= Wmana
-                && Minions.Count >= kinm.Item("laneclearWminminions", true).GetValue<Slider>().Value) {
+                && MINIONS.Count >= kinm.Item("laneclearWminminions", true).GetValue<Slider>().Value) {
                 W.Cast();                
+            }
+            if (kinm.Item("laneclearFAST", true).GetValue<Boolean>() && Manapercent >= clearFAST && MINIONS.Count >= kinm.Item("laneclearFASTminions", true).GetValue<Slider>().Value) {
+                if (W.IsReady()) { W.Cast(); }
+                if (Q.IsReady()) { Q.Cast(Game.CursorPos); }
             }
         }
 
@@ -283,44 +296,50 @@ namespace Kinky_Kindred {
 
         #region MISC FUNCTIONS
         static void checksmitecamps() {
-            if (smite == SpellSlot.Unknown || Player.Spellbook.CanUseSpell(smite) != SpellState.Ready) { return; }
-            var minion = MinionManager.GetMinions(E.Range, MinionTypes.All, MinionTeam.All, MinionOrderTypes.MaxHealth).Find(x => x.CharData.BaseSkinName.ToLower().Contains("sru"));
+            var minion = MinionManager.GetMinions(E.Range, MinionTypes.All, MinionTeam.All, MinionOrderTypes.MaxHealth).Find(x => x.CharData.BaseSkinName.ToLower().Contains("sru") && !x.CharData.BaseSkinName.ToLower().Contains("mini"));
             if (minion == null) { return; }
+            Drawing.DrawText(Drawing.Width * 0.45f, Drawing.Height * 0.10f, Color.GreenYellow, "Mob is: " + minion.CharData.BaseSkinName);
+            if (smite == SpellSlot.Unknown || Player.Spellbook.CanUseSpell(smite) != SpellState.Ready) { return; }
             var damage = Player.GetSummonerSpellDamage(minion, Damage.SummonerSpell.Smite);
 
-            if (kinm.Item("always_smite_red", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("red") && damage > minion.Health) {
-                Player.Spellbook.CastSpell(smite, minion); return;
+            if (minion.CharData.BaseSkinName.ToLower().Contains("red")) {
+                if (kinm.Item("always_smite_red", true).GetValue<Boolean>() && damage > minion.Health) {
+                    Player.Spellbook.CastSpell(smite, minion);
+                }
+                if (((Player.Health / Player.MaxHealth) * 100) <= kinm.Item("smite_r_if__dying", true).GetValue<Slider>().Value) {
+                    Player.Spellbook.CastSpell(smite, minion);
+                }
+                if (kinm.Item("smite_brf_til_lvlonoff", true).GetValue<Boolean>() && Player.Level <= kinm.Item("smite_brf_til_lvl", true).GetValue<Slider>().Value) {
+                    Player.Spellbook.CastSpell(smite, minion);
+                }
             }
-            if (kinm.Item("smite_r_if__dying", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("red") &&
-                ((Player.Health / Player.MaxHealth) * 100) <= kinm.Item("smite_r_if__dying", true).GetValue<Slider>().Value) {
-                Player.Spellbook.CastSpell(smite, minion); return;
-            }
-            if (kinm.Item("always_smite_blue", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("blue") && damage > minion.Health) {
-                Player.Spellbook.CastSpell(smite, minion); return;
+
+            if (minion.CharData.BaseSkinName.ToLower().Contains("blue")) {
+                if (kinm.Item("always_smite_blue", true).GetValue<Boolean>() && damage > minion.Health) { Player.Spellbook.CastSpell(smite, minion); }
+                if (kinm.Item("smite_brf_til_lvlonoff", true).GetValue<Boolean>() && Player.Level <= kinm.Item("smite_brf_til_lvl", true).GetValue<Slider>().Value) {
+                    Player.Spellbook.CastSpell(smite, minion);
+                }
             }
             //always smite frog from start to get poison buffs
-            if (kinm.Item("always_smite_frog", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("gromp")) {
+            if (minion.CharData.BaseSkinName.ToLower().Contains("gromp")) {
+                if (kinm.Item("always_smite_frog", true).GetValue<Boolean>()) { Player.Spellbook.CastSpell(smite, minion); }
+                if (kinm.Item("smite_brf_til_lvlonoff", true).GetValue<Boolean>() && Player.Level <= kinm.Item("smite_brf_til_lvl", true).GetValue<Slider>().Value) {
+                    Player.Spellbook.CastSpell(smite, minion);
+                }
+            }
+            if (kinm.Item("always_smite_golems", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("krug") && damage > minion.Health) {
                 Player.Spellbook.CastSpell(smite, minion); return;
             }
-            if (kinm.Item("smite_brf_til_lvl", true).GetValue<Boolean>() &&
-                (minion.CharData.BaseSkinName.ToLower().Contains("gromp") ||
-                minion.CharData.BaseSkinName.ToLower().Contains("red") ||
-                minion.CharData.BaseSkinName.ToLower().Contains("blue")) && Player.Level <= kinm.Item("smite_brf_til_lvl", true).GetValue<Slider>().Value) {
+            if (kinm.Item("always_smite_dragon", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("dragon") && damage > minion.Health) {
                 Player.Spellbook.CastSpell(smite, minion); return;
             }
             if (kinm.Item("always_smite_wolf", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("murkwolf") && damage > minion.Health) {
-                Player.Spellbook.CastSpell(smite, minion); return;
-            }
-            if (kinm.Item("always_smite_golems", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("krug") && damage > minion.Health) {
                 Player.Spellbook.CastSpell(smite, minion); return;
             }
             if (kinm.Item("always_smite_wraiths", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("razorbeak") && damage > minion.Health) {
                 Player.Spellbook.CastSpell(smite, minion); return;
             }
             if (kinm.Item("always_smite_baron", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("baron") && damage > minion.Health) {
-                Player.Spellbook.CastSpell(smite, minion); return;
-            }
-            if (kinm.Item("always_smite_dragon", true).GetValue<Boolean>() && minion.CharData.BaseSkinName.ToLower().Contains("dragon") && damage > minion.Health) {
                 Player.Spellbook.CastSpell(smite, minion); return;
             }
         }
@@ -397,22 +416,90 @@ namespace Kinky_Kindred {
             return null;
         }
 
-        static void Event_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args) {
-            if (Player.IsDead || !E.IsReady()) { return; }
+        void Obj_AI_Hero_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args) {
 
-            if (sender is Obj_AI_Hero && sender.IsEnemy && args.Target != null && args.Target.NetworkId == Player.NetworkId) {
-                var enemy = (Obj_AI_Hero)sender;
-                var slot = enemy.GetSpellSlot(args.SData.Name);
-                if (slot == SpellSlot.Unknown) { return; }
-                //ignite...
-                if (slot == enemy.GetSpellSlot("SummonerDot")) {
-                    var dmgonsoul = (float)enemy.GetSummonerSpellDamage(Player, Damage.SummonerSpell.Ignite);
-                    if (dmgonsoul > Player.Health && R.IsReady()) { R.Cast(); }
+            //Patented by XcxooxL
+            if (!sender.IsEnemy || !sender.IsValidTarget() || !sender.IsVisible) return;
+
+            if (args.Target != null && !args.Target.IsAlly) return;
+            double damage = 0;
+            //if it is an Auto Attack !
+            if (args.SData.IsAutoAttack()) {
+                var Target = args.Target as Obj_AI_Hero;
+                if (Target == null)
+                    return; //must be a champion
+                if (args.SData.Name.ToLower().Contains("crit")) {
+                    damage += sender.GetAutoAttackDamage(Target) * 2;
+                    //Console.WriteLine("Critical " + damage);
+                    if (sender.InventoryItems.Any(item => item.Id.Equals(3031))) {
+                        Console.WriteLine("Infinity Edge");
+                        damage += damage * 1.25;
+                    }
+                    //Infinity edge
+                } else {
+                    damage += sender.GetAutoAttackDamage(Target, true);
                 }
-                if (Player.HealthPercent <= kinm.Item("saveallieswhen", true).GetValue<Slider>().Value && !R.IsInRange(enemy.ServerPosition)) {
-                    R.Cast(Player.ServerPosition);
+                damage += 2; //to be on the safe side
+                Add(Target, damage, sender.Distance(Target) / args.SData.MissileSpeed + 1 / sender.AttackDelay);
+                Console.WriteLine(
+                    "Target : " + Target.Name + "Damage : " + damage + " Time To Hit : " +
+                    sender.Distance(Target) / args.SData.MissileSpeed * 1000);
+
+            } else //if its a Spell
+              {
+                float delay = 0;
+                var missileSpeed = args.SData.MissileSpeed;
+                foreach (var spellInfo in
+                    SpellDatabase.Spells.Where(spellInfo => spellInfo.spellName.Equals(args.SData.Name))) {
+                    if (spellInfo.spellType.Equals(SpellType.Line)) {
+                        _myPoly = new Geometry.Polygon.Rectangle(
+                            args.Start, args.Start.Extend(args.End, spellInfo.range), spellInfo.radius);
+                    } else if (spellInfo.spellType.Equals(SpellType.Circular)) {
+
+                        var pos = sender.Distance(args.End) > spellInfo.range
+                            ? sender.Position.Extend(args.End, spellInfo.range)
+                            : args.End;
+                        _myPoly = new Geometry.Polygon.Circle(pos, spellInfo.radius);
+                    }
+                    missileSpeed = spellInfo.projectileSpeed;
+                    delay += spellInfo.spellDelay;
+                    break;
+                }
+
+                //Patented by xcxooxl ALL OF THIS IS MINE ! YOU WANT IT? CREDIT ME!
+
+                if (sender is Obj_AI_Hero) {
+                    var enemy = sender as Obj_AI_Hero;
+                    foreach (var ally in TrackList) {
+                        var timeToHit = delay + ally.Distance(args.Start) / missileSpeed * 1000 +
+                                        args.SData.ChannelDuration + args.SData.DelayTotalTimePercent * -1;
+                        if (args.SData.TargettingType.Equals(SpellDataTargetType.Unit)) //Targeted
+                        {
+                            damage += enemy.GetDamageSpell(args.Target as Obj_AI_Base, args.Slot).CalculatedDamage;
+                            Add(ally, damage, timeToHit);
+                        }
+
+
+                        Console.WriteLine(
+                            "Spellname" + args.SData.Name + " Time to hit " + timeToHit + "MissileSpeed " + missileSpeed);
+
+                        var futurePos = Prediction.GetPrediction(ally, timeToHit / 1000).UnitPosition;
+                        futurePosCircle = new Geometry.Polygon.Circle(futurePos, 125);
+                        if (_myPoly.IsInside(futurePos)) {
+                            damage += enemy.GetDamageSpell(ally, args.Slot).CalculatedDamage;
+                            Add(ally, damage, timeToHit);
+                        }
+                        Utility.DelayAction.Add(
+                            (int)(timeToHit + 1200), () => {
+                                futurePosCircle = null;
+                                _myPoly = null;
+                            }); //stop drawing polygons
+
+                    }
                 }
             }
+
+            //Patented by XcxooxL
         }
 
         static bool hasundyingbuff(Obj_AI_Hero target) {
